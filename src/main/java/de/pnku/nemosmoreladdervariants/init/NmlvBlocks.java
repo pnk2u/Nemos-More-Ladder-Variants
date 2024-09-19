c67c264c16573bbb0b0895e5aa17933914490734
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-09-19 02:24:31 +0200

    fix block registration for 1.20.1

--- src/main/java/de/pnku/nemosmoreladdervariants/init/NmlvBlocks.java
+++ src/main/java/de/pnku/nemosmoreladdervariants/init/NmlvBlocks.java
@@ -23,26 +23,26 @@ public class NmlvBlocks {
     public static void register() {}
 
     // Ladder Variant Blocks (Reverse Order)
-    public static final Block WARPED_LADDER = registerLadderBlock("warped_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER).sound(SoundType.NETHER_WOOD)));
+    public static final Block WARPED_LADDER = registerLadderBlock("warped_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER).sound(SoundType.NETHER_WOOD)));
 
-    public static final Block CRIMSON_LADDER = registerLadderBlock("crimson_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER).sound(SoundType.NETHER_WOOD)));
+    public static final Block CRIMSON_LADDER = registerLadderBlock("crimson_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER).sound(SoundType.NETHER_WOOD)));
 
-    public static final Block BOUND_BAMBOO_LADDER = registerLadderBlock("bound_bamboo_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER).sound(SoundType.BAMBOO_WOOD)));
+    public static final Block BOUND_BAMBOO_LADDER = registerLadderBlock("bound_bamboo_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER).sound(SoundType.BAMBOO_WOOD)));
 
-    public static final Block BAMBOO_LADDER = registerLadderBlock("bamboo_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER).sound(SoundType.BAMBOO_WOOD)));
+    public static final Block BAMBOO_LADDER = registerLadderBlock("bamboo_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER).sound(SoundType.BAMBOO_WOOD)));
 
-    public static final Block CHERRY_LADDER = registerLadderBlock("cherry_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER).sound(SoundType.CHERRY_WOOD)));
+    public static final Block CHERRY_LADDER = registerLadderBlock("cherry_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER).sound(SoundType.CHERRY_WOOD)));
 
-    public static final Block MANGROVE_LADDER = registerLadderBlock("mangrove_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER)));
+    public static final Block MANGROVE_LADDER = registerLadderBlock("mangrove_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER)));
 
-    public static final Block DARK_OAK_LADDER = registerLadderBlock("dark_oak_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER)));
+    public static final Block DARK_OAK_LADDER = registerLadderBlock("dark_oak_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER)));
 
-    public static final Block ACACIA_LADDER = registerLadderBlock("acacia_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER)));
+    public static final Block ACACIA_LADDER = registerLadderBlock("acacia_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER)));
 
-    public static final Block JUNGLE_LADDER = registerLadderBlock("jungle_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER)));
+    public static final Block JUNGLE_LADDER = registerLadderBlock("jungle_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER)));
 
-    public static final Block BIRCH_LADDER = registerLadderBlock("birch_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER)));
+    public static final Block BIRCH_LADDER = registerLadderBlock("birch_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER)));
 
-    public static final Block SPRUCE_LADDER = registerLadderBlock("spruce_ladder", new LadderBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.LADDER)));
+    public static final Block SPRUCE_LADDER = registerLadderBlock("spruce_ladder", new LadderBlock(BlockBehaviour.Properties.copy(Blocks.LADDER)));
 
 }
