de126202821a7e72f668fd474db0be986a972be5
pnku <159317511+pnk2u@users.noreply.github.com>
2025-06-17 09:39:53 +0200

    Compatibility with 1.21.6

--- src/main/java/de/pnku/nemosmoreladdervariants/NemosMoreLadderVariantsClient.java
+++ src/main/java/de/pnku/nemosmoreladdervariants/NemosMoreLadderVariantsClient.java
@@ -4,7 +4,7 @@ import de.pnku.nemosmoreladdervariants.init.NmlvBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 
@@ -14,10 +14,36 @@ import static de.pnku.nemosmoreladdervariants.NemosMoreLadderVariants.isNemosCar
 public class NemosMoreLadderVariantsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        String mcVersion = FabricLoader.getInstance().getModContainer("minecraft").get().getMetadata().getVersion().getFriendlyString();
+        boolean isLegacy = mcVersion.contains("1.21.4") || mcVersion.contains("1.21.5");
+        boolean isDev = FabricLoader.getInstance().isDevelopmentEnvironment();
         if (!isNemosCarpentryLoaded) {
             for (Block ladderBlock : NmlvBlocks.more_ladders) {
-                BlockRenderLayerMap.INSTANCE.putBlock(ladderBlock, RenderType.cutout());
+                if (isLegacy) {legacyAddToRenderLayerMap(ladderBlock);}
+                else {addToRenderLayerMap(ladderBlock, isDev);}
             }
+        }
+    }
+
+    private void legacyAddToRenderLayerMap(Block block) {
+        try {
+            Class<?> legacyBlockRenderLayerMap = Class.forName("net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap");
+            legacyBlockRenderLayerMap.getMethod("putBlock", Block.class, RenderType.class)
+                    .invoke(legacyBlockRenderLayerMap.getField("INSTANCE").get(null), block, RenderType.cutout());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addToRenderLayerMap(Block block, boolean isDev) {
+        try {
+            Class<?> blockRenderLayerMap = Class.forName("net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap");
+            String chunkSectionLayerClassName = isDev ? "net.minecraft.client.renderer.chunk.ChunkSectionLayer" : "net.minecraft.class_11515";
+            Class<?> chunkSectionLayerClass = Class.forName(chunkSectionLayerClassName);
+            blockRenderLayerMap.getMethod("putBlock", Block.class, chunkSectionLayerClass)
+                    .invoke(null, block, chunkSectionLayerClass.getEnumConstants()[2]);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
